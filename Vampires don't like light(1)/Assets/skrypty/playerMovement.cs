93e38c1280d27cdb3e39455515e2c9648b55fbb9
AoDem0 <dsw52708@student.dsw.edu.pl>
2024-10-30 21:23:34 +0100

    making animations
    player movment and idle animations, some simple level changes (fixing cause lvl changed after making animations)

--- Vampires don't like light(1)/Assets/skrypty/playerMovement.cs
+++ Vampires don't like light(1)/Assets/skrypty/playerMovement.cs
@@ -9,6 +9,7 @@ public class playerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private float moveX;
     private Vector2 vec;
+    public Animator animator;
     
     // Start is called before the first frame update
     void Start()
@@ -19,11 +20,17 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        moveX = Input.GetAxis("Horizontal");
+        if (Input.GetAxis("Horizontal") != 0){
+            moveX = Input.GetAxis("Horizontal");
+            animator.SetBool("isMoving", true);
+            vec = new Vector2(moveX, 0);
+            var movement = vec.normalized * speed;
+            rb.velocity = movement;
         
-        vec = new Vector2(moveX, 0);
-        var movement = vec.normalized * speed;
-        rb.velocity = movement;
-        
+        }
+        else{
+            animator.SetBool("isMoving", false);
+        }
+
     }
 }
