52646d3110c737f7ef4a1b2c0f023093ae0b1d28
AoDem0 <dsw52708@student.dsw.edu.pl>
2024-10-31 14:11:35 +0100

    camera
    changing camera code that it only follows player when player is in some position

--- Vampires don't like light(1)/Assets/skrypty/followPlayer.cs
+++ Vampires don't like light(1)/Assets/skrypty/followPlayer.cs
@@ -1,23 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class followPlayer : MonoBehaviour
 {
     public Transform player;
     private Vector3 offset;
     public float followSpeed = 2f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        offset = new Vector3(player.position.x, player.position.y + 2.6f , -10f);
-        transform.position = Vector3.Slerp(transform.position, offset, followSpeed);
+
+    void FixedUpdate()
+    { 
+        if(player.transform.position.x > -16.87 & player.transform.position.x < 123.0709){
+            offset = new Vector3(player.position.x, player.position.y + 2.6f , -10f);
+            transform.position = Vector3.Slerp(transform.position, offset, followSpeed);
+        }
     }
 }
     
