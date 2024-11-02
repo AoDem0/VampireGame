3814c51d681a202b568bc0d2fb1a997e0c4b69d3
AoDem0 <dsw52708@student.dsw.edu.pl>
2024-11-02 18:06:38 +0100

    music and tests
    adding background music and some play tests

--- Vampires don't like light(1)/Assets/skrypty/audioManager.cs
+++ Vampires don't like light(1)/Assets/skrypty/audioManager.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class audioManager : MonoBehaviour
 {
+    public AudioSource musicSource;
+
     public AudioSource SFXSource;
     public AudioClip burn;
+    public AudioClip backMusic;
+
+    private void Awake(){
+        DontDestroyOnLoad(gameObject);
+    }
+    private void Start(){
+        musicSource.clip = backMusic;
+        musicSource.Play();
+    }
     public void PlaySFX(AudioClip clip){
         SFXSource.PlayOneShot(clip);
     }
